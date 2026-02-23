5b00dd65f7bf7c147b22a577e1deea7f7644b0b9
Gaita Iustin Florian <iustingaita.f@gmail.com>
2026-02-23 12:24:35 +0200

    initial commit: spring boot learning & development project

---
+++ src/test/java/com/gifprojects/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gifprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
